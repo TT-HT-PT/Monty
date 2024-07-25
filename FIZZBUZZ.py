98c442669c38fc19a827ef0dc0358443cd938f87
TT-HT-PT <randomandgeneral@gmail.com>
2024-07-24 23:20:11 -0400

    Add files via upload

--- FIZZBUZZ.py
+++ FIZZBUZZ.py
@@ -5,20 +5,24 @@ Created on Wed Jul 24 11:47:55 2024
 
 @author: hsavjani
 """
-
-import math
 while True:
+    name = str(input("Please enter your name  "))        
     try:
-        name = input("Please enter your name  ")
         num = int(input("Please enter a number  "))
-        if num < 0:
-            raise Exception("Please enter a valid number")
-    finally:
-        if num % 3 == 0 and num % 5 == 0:
-            print("It's a FIZZBUZZ")
-        elif num % 3 == 0:
-            print("It's a FIZZ")
-        elif num % 5 == 0:
-            print("It's a BUZZ")
-        else:
-            print("Its neither")
+    except:
+        print("Please use a numeric digit")  
+        continue
+    if num < 1:
+        print("Please enter a valid number")
+        continue
+    break
+            
+if num % 3 == 0 and num % 5 == 0:
+    print("It's a FIZZBUZZ")
+elif num % 3 == 0:
+    print("It's a FIZZ")
+elif num % 5 == 0:
+    print("It's a BUZZ")
+else:
+    print("Its neither")
+
